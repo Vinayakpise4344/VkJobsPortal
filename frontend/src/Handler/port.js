fb68b609d6b9520994e941884739f3e282f69d98
Vinayak <pisevinayak56@gmail.com>
2025-01-04 01:05:24 +0530

    first commi

--- frontend/src/Handler/port.js
+++ frontend/src/Handler/port.js
@@ -1,4 +1,4 @@
-export const User_api="http://localhost:1001/api/v1/user";
-export const Job_api="http://localhost:1001/api/v1/job";
-export const application_api="http://localhost:1001/api/v1/application";
-export const company_api="http://localhost:1001/api/v1/company";
+export const User_api="https://vkjobssportal.onrender.com/api/v1/user";
+export const Job_api="https://vkjobssportal.onrender.com/api/v1/job";
+export const application_api="https://vkjobssportal.onrender.com/api/v1/application";
+export const company_api="https://vkjobssportal.onrender.com/api/v1/company";
